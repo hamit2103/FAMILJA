5a414fe6d1798ddf1e00253e289c9114fa2f8bc9
hamit2103 <B.pajaziti111@web.de>
2026-09-20 01:14:47 +0200

    Refresh cache for native prayer alarms

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pajaziti-v9";
+const CACHE = "pajaziti-v10";
 const SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./app-config.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
