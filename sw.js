d5a6264d7132a5b346888a3669dff36bbcc22dcf
hamit2103 <B.pajaziti111@web.de>
2026-09-20 00:56:51 +0200

    Refresh cache for storage meter

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pajaziti-v7";
+const CACHE = "pajaziti-v8";
 const SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./app-config.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
