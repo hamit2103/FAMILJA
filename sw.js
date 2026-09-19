efc43b632abb4a45da4032372ead4c188b6eea15
hamit2103 <B.pajaziti111@web.de>
2026-09-20 00:46:41 +0200

    Refresh app cache for online counter

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pajaziti-v5";
+const CACHE = "pajaziti-v6";
 const SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./app-config.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
