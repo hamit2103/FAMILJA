69a5f19a11c2a05bd078f51ae097ac47340a2b84
hamit2103 <B.pajaziti111@web.de>
2026-09-20 01:21:00 +0200

    Refresh cache for multilingual app

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pajaziti-v10";
+const CACHE = "pajaziti-v11";
 const SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./app-config.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
