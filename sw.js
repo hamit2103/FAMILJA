f09d313c4e5cb0a145e9ad7b626914218dc7328b
hamit2103 <B.pajaziti111@web.de>
2026-09-20 04:12:05 +0200

    Refresh cache for Shtime TV category redesign

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pajaziti-v22";
+const CACHE = "pajaziti-v23";
 const SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./app-config.js", "./manifest.webmanifest", "./icon.svg", "./games.js", "./tv.js"];
 
 self.addEventListener("install", (event) => {
