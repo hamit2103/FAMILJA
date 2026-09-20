a155c3adab93c54d89c2e62773ffcccf53a477d0
hamit2103 <B.pajaziti111@web.de>
2026-09-20 12:44:23 +0200

    Bump cache for direct family login

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pajaziti-v24";
+const CACHE = "pajaziti-v26";
 const SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./app-config.js", "./manifest.webmanifest", "./icon.svg", "./games.js", "./tv.js"];
 
 self.addEventListener("install", (event) => {
