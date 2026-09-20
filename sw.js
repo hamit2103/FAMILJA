93f220d370a63e04877caac00c43a698fc305d33
hamit2103 <B.pajaziti111@web.de>
2026-09-20 03:44:01 +0200

    Refresh cache for Degerxhik fullscreen redesign

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pajaziti-v17";
+const CACHE = "pajaziti-v18";
 const SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./app-config.js", "./manifest.webmanifest", "./icon.svg", "./games.js"];
 
 self.addEventListener("install", (event) => {
