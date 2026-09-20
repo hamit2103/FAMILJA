889548cf21c9aeda73f100a6031e14eef7cc45e0
hamit2103 <B.pajaziti111@web.de>
2026-09-20 15:06:56 +0200

    Deploy latest Tetris touch controls

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pajaziti-v39";
+const CACHE = "pajaziti-v40";
 const SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./app-config.js", "./manifest.webmanifest", "./icon.svg", "./games.js", "./tv.js", "./radio.js"];
 
 self.addEventListener("install", (event) => {
