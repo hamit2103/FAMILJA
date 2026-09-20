7a9bf8497051967258b1649b65d2197ee11a684e
hamit2103 <B.pajaziti111@web.de>
2026-09-20 20:44:27 +0200

    Refresh cache for five-heart War progression

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pajaziti-v55";
+const CACHE = "pajaziti-v56";
 const SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./app-config.js", "./manifest.webmanifest", "./icon.svg", "./games.js", "./war.js", "./tv.js", "./radio.js"];
 
 self.addEventListener("install", (event) => {
