62e8b8c2fb98270428edc3587e69387a4066865f
hamit2103 <B.pajaziti111@web.de>
2026-09-20 15:47:32 +0200

    Refresh cache for menu ordering

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pajaziti-v42";
+const CACHE = "pajaziti-v43";
 const SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./app-config.js", "./manifest.webmanifest", "./icon.svg", "./games.js", "./tv.js", "./radio.js"];
 
 self.addEventListener("install", (event) => {
