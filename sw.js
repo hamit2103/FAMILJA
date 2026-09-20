9d09e90bb27c6dde5afe1a08639a50f4de8a0fd1
hamit2103 <B.pajaziti111@web.de>
2026-09-20 13:00:39 +0200

    Refresh app cache after Radio fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pajaziti-v26";
+const CACHE = "pajaziti-v28";
 const SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./app-config.js", "./manifest.webmanifest", "./icon.svg", "./games.js", "./tv.js", "./radio.js"];
 
 self.addEventListener("install", (event) => {
