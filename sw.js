f2c864282ab9a94c73c9a49f4996b01bdddf07c1
hamit2103 <B.pajaziti111@web.de>
2026-09-20 21:23:02 +0200

    Refresh cache for War user leaderboard and animations

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pajaziti-v58";
+const CACHE = "pajaziti-v59";
 const SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./app-config.js", "./manifest.webmanifest", "./icon.svg", "./games.js", "./war.js", "./tv.js", "./radio.js"];
 
 self.addEventListener("install", (event) => {
