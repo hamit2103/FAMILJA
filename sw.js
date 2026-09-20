0aa607c0284531fec45de427f41f5e2210b6b717
hamit2103 <B.pajaziti111@web.de>
2026-09-20 14:32:14 +0200

    Refresh cache for Tetris

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pajaziti-v31";
+const CACHE = "pajaziti-v34";
 const SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./app-config.js", "./manifest.webmanifest", "./icon.svg", "./games.js", "./tv.js", "./radio.js"];
 
 self.addEventListener("install", (event) => {
