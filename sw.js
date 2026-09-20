36efb5e8e1d0912ce26daed15ce0b29f86c9183c
hamit2103 <B.pajaziti111@web.de>
2026-09-20 15:25:02 +0200

    Refresh cache for Information notifications

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pajaziti-v40";
+const CACHE = "pajaziti-v42";
 const SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./app-config.js", "./manifest.webmanifest", "./icon.svg", "./games.js", "./tv.js", "./radio.js"];
 
 self.addEventListener("install", (event) => {
