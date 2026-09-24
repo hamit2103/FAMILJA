e6ba4d23e889a448235afa09b29b5996aec79b7f
hamit2103 <B.pajaziti111@web.de>
2026-09-24 12:58:40 +0200

    Expose stable Android device id for admin blocking

--- firetv/app/src/main/java/com/pajaziti/familja/AppInfoBridge.java
+++ firetv/app/src/main/java/com/pajaziti/familja/AppInfoBridge.java
@@ -1,12 +1,26 @@
 package com.pajaziti.familja;
 
 import android.webkit.JavascriptInterface;
+import android.provider.Settings;
 
 public class AppInfoBridge {
     private final MainActivity activity;
 
     public AppInfoBridge(MainActivity activity) {
         this.activity = activity;
+    }
+
+    @JavascriptInterface
+    public String getStableDeviceId() {
+        try {
+            String id = Settings.Secure.getString(
+                activity.getContentResolver(),
+                Settings.Secure.ANDROID_ID
+            );
+            return id == null ? "" : id;
+        } catch (Exception ignored) {
+            return "";
+        }
     }
 
     @JavascriptInterface
