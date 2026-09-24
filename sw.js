97551b26f522bcdca70fa27ac48cf6e814df1e96
hamit2103 <B.pajaziti111@web.de>
2026-09-24 11:37:07 +0200

    Bump DIAMOND 5.78 admin upload fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "diamond-v97";
+const CACHE = "diamond-v98";
 const SHELL = ["./", "./index.html", "./admin.html", "./admin-manifest.webmanifest", "./styles.css?v=87", "./app.js?v=82", "./app-config.js", "./manifest.webmanifest", "./angel-icon.jpg", "./sports.js?v=8", "./games.js?v=93", "./tv.js?v=52", "./radio.js?v=54", "./quran.js?v=1", "./prayer-extras.js?v=2", "./news.js?v=3"];
 
 self.addEventListener("install", (event) => {
