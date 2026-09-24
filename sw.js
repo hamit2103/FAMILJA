199dae712040c634f2fd0a8369eb6f67ada133d5
hamit2103 <B.pajaziti111@web.de>
2026-09-24 13:39:45 +0200

    DIAMOND 5.84: fit Home menu to one phone screen
    * Fit home menu dynamically to phone viewport
    
    * Compact Home layout to fit one phone screen
    
    * Bump DIAMOND 5.84 compact home menu
    
    * Bump DIAMOND 5.84 compact home menu
    
    * Bump DIAMOND 5.84 compact home menu
    
    * Bump DIAMOND 5.84 compact home menu
    
    * Bump DIAMOND 5.84 compact home menu

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "diamond-v103";
-const SHELL = ["./", "./index.html", "./admin.html", "./admin-manifest.webmanifest", "./styles.css?v=90", "./app.js?v=103", "./app-config.js", "./manifest.webmanifest", "./angel-icon.jpg", "./sports.js?v=8", "./games.js?v=94", "./tv.js?v=52", "./radio.js?v=54", "./quran.js?v=1", "./prayer-extras.js?v=2", "./news.js?v=3"];
+const CACHE = "diamond-v104";
+const SHELL = ["./", "./index.html", "./admin.html", "./admin-manifest.webmanifest", "./styles.css?v=91", "./app.js?v=104", "./app-config.js", "./manifest.webmanifest", "./angel-icon.jpg", "./sports.js?v=8", "./games.js?v=94", "./tv.js?v=52", "./radio.js?v=54", "./quran.js?v=1", "./prayer-extras.js?v=2", "./news.js?v=3"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)));
