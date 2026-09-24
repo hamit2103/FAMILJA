0314bf1f6bc2048becefe0aed78145e4dd4fff9e
hamit2103 <B.pajaziti111@web.de>
2026-09-24 20:48:54 +0200

    DIAMOND 5.91: complete manual update build

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "diamond-v118";
-const SHELL = ["./", "./index.html", "./admin.html", "./admin-manifest.webmanifest", "./styles.css?v=98", "./app.js?v=113", "./app-config.js", "./manifest.webmanifest", "./angel-icon.jpg", "./sports.js?v=8", "./games.js?v=94", "./tv.js?v=55", "./radio.js?v=54", "./quran.js?v=1", "./prayer-extras.js?v=2", "./news.js?v=3"];
+const CACHE = "diamond-v119";
+const SHELL = ["./", "./index.html", "./admin.html", "./admin-manifest.webmanifest", "./styles.css?v=99", "./app.js?v=114", "./app-config.js", "./manifest.webmanifest", "./angel-icon.jpg", "./sports.js?v=8", "./games.js?v=94", "./tv.js?v=55", "./radio.js?v=54", "./quran.js?v=1", "./prayer-extras.js?v=2", "./news.js?v=3"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)));
