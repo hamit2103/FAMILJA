e043c7a5ddf1d3beb3035c4bddaaf3462299cd5c
hamit2103 <B.pajaziti111@web.de>
2026-09-25 14:00:37 +0200

    Release Diamond Adventure in 6.12

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "diamond-v142";
-const SHELL = ["./", "./index.html", "./admin.html", "./admin-manifest.webmanifest", "./styles.css?v=107", "./app.js?v=119", "./app-config.js", "./manifest.webmanifest", "./angel-icon.jpg", "./sports.js?v=11", "./games.js?v=100", "./tv.js?v=55", "./radio.js?v=54", "./quran.js?v=1", "./prayer-extras.js?v=2", "./news.js?v=3", "./diet.js?v=8", "./kingdom.js?v=1", "./uck.js?v=1", "./diamond-run.js?v=4"];
+const CACHE = "diamond-v143";
+const SHELL = ["./", "./index.html", "./admin.html", "./admin-manifest.webmanifest", "./styles.css?v=107", "./app.js?v=119", "./app-config.js", "./manifest.webmanifest", "./angel-icon.jpg", "./sports.js?v=11", "./games.js?v=101", "./tv.js?v=55", "./radio.js?v=54", "./quran.js?v=1", "./prayer-extras.js?v=2", "./news.js?v=3", "./diet.js?v=8", "./kingdom.js?v=1", "./uck.js?v=1", "./diamond-run.js?v=4", "./diamond-adventure.js?v=1"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)));
