56663dd972b352551e1a42db130a5e7b1279d0d8
hamit2103 <B.pajaziti111@web.de>
2026-09-25 03:47:27 +0200

    Refresh cache for UCK game

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "diamond-v133";
-const SHELL = ["./", "./index.html", "./admin.html", "./admin-manifest.webmanifest", "./styles.css?v=105", "./app.js?v=118", "./app-config.js", "./manifest.webmanifest", "./angel-icon.jpg", "./sports.js?v=11", "./games.js?v=95", "./tv.js?v=55", "./radio.js?v=54", "./quran.js?v=1", "./prayer-extras.js?v=2", "./news.js?v=3", "./diet.js?v=8", "./kingdom.js?v=1"];
+const CACHE = "diamond-v134";
+const SHELL = ["./", "./index.html", "./admin.html", "./admin-manifest.webmanifest", "./styles.css?v=105", "./app.js?v=118", "./app-config.js", "./manifest.webmanifest", "./angel-icon.jpg", "./sports.js?v=11", "./games.js?v=96", "./tv.js?v=55", "./radio.js?v=54", "./quran.js?v=1", "./prayer-extras.js?v=2", "./news.js?v=3", "./diet.js?v=8", "./kingdom.js?v=1", "./uck.js?v=1"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)));
