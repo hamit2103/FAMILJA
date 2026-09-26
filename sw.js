b812261872ead2ad3877e404511169df79444a78
hamit2103 <B.pajaziti111@web.de>
2026-09-26 12:49:04 +0200

    Refresh cache for DIAMOND 6.20

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "diamond-v152";
-const SHELL = ["./", "./index.html", "./admin.html", "./admin-manifest.webmanifest", "./styles.css?v=108", "./app.js?v=123", "./app-config.js", "./manifest.webmanifest", "./angel-icon.jpg", "./sports.js?v=11", "./games.js?v=106", "./tv.js?v=56", "./radio.js?v=57", "./recipes.js?v=1", "./quran.js?v=1", "./prayer-extras.js?v=2", "./news.js?v=3", "./nearby.js?v=1", "./diet.js?v=8", "./kingdom.js?v=2", "./uck.js?v=1", "./diamond-run.js?v=4", "./diamond-adventure.js?v=1", "./diamond-nations.js?v=2"];
+const CACHE = "diamond-v153";
+const SHELL = ["./", "./index.html", "./admin.html", "./admin-manifest.webmanifest", "./styles.css?v=108", "./app.js?v=124", "./app-config.js", "./manifest.webmanifest", "./angel-icon.jpg", "./sports.js?v=11", "./games.js?v=106", "./tv.js?v=56", "./radio.js?v=57", "./recipes.js?v=1", "./quran.js?v=1", "./prayer-extras.js?v=2", "./news.js?v=3", "./nearby.js?v=2", "./diet.js?v=8", "./kingdom.js?v=2", "./uck.js?v=1", "./diamond-run.js?v=4", "./diamond-adventure.js?v=1", "./diamond-nations.js?v=2"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)));
